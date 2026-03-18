0fcdd327bafac51acd99b3e51ed3f07bdfcfa187
Joshua Cox <joshua.n.cox@gmail.com>
2026-03-18 12:47:45 -0700

    Add cache bust to latent loader

--- src/usefulness/load_latent_abs.py
+++ src/usefulness/load_latent_abs.py
@@ -1,8 +1,8 @@
 import os
+import random
 import folder_paths
 import hashlib
 import safetensors.torch
-from PIL import Image
 
 class LoadLatentFromAbs:
     @classmethod
@@ -12,9 +12,9 @@ class LoadLatentFromAbs:
                 "latent_path": ("STRING", {"default": ""}),
             }
         }
-    
+
     CATEGORY = "latent"
-    RETURN_TYPES = ("LATENT",)
+    RETURN_TYPES = ("LATENT", "INT")
     FUNCTION = "load_latent"
     TITLE = "Load Latent (Absolute Path)"
 
@@ -22,19 +22,19 @@ class LoadLatentFromAbs:
         # Check if path exists
         if not os.path.exists(latent_path):
             raise ValueError(f"Latent file not found at path: {latent_path}")
-        
+
         latent = safetensors.torch.load_file(latent_path, device="cpu")
         multiplier = 1.0
         if "latent_format_version_0" not in latent:
             multiplier = 1.0 / 0.18215
         samples = {"samples": latent["latent_tensor"].float() * multiplier}
-        return (samples, )
+        return (samples, random.randint(0, 2**31 - 1))
 
     @classmethod
     def IS_CHANGED(s, latent_path):
         image_path = folder_paths.get_annotated_filepath(latent_path)
         m = hashlib.sha256()
-        with open(image_path, 'rb') as f:
+        with open(image_path, "rb") as f:
             m.update(f.read())
         return m.digest().hex()
 
