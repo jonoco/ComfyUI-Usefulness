0105c8a918f1c59960f6441a0735ae5f24db7e3a
Joshua Cox <joshua.n.cox@gmail.com>
2026-03-19 00:35:12 -0700

    Make latent selector generalized

--- src/usefulness/select_latent.py
+++ src/usefulness/select_latent.py
@@ -2,12 +2,14 @@ import torch
 from comfy.sd import VAE
 from nodes import common_ksampler
 
+
 class LatentSelector:
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
                 "latent": ("LATENT",),
+                "dim": ("INT", {"default": 2, "min": 0, "max": 5, "step": 1}),
                 "start_index": ("INT", {"default": 0, "min": -999999, "max": 99999, "step": 4}),
                 "end_index": ("INT", {"default": 0, "min": -999999, "max": 99999, "step": 4}),
                 "use_end_index": ("BOOLEAN", {"default": True}),
@@ -17,35 +19,27 @@ class LatentSelector:
     RETURN_TYPES = ("LATENT", "INT")
     RETURN_NAMES = ("latent", "selected_count")
     FUNCTION = "select_latent"
-    CATEGORY = "Wan/latent"
+    CATEGORY = "latent"
 
-    def select_latent(self, latent, start_index, end_index, use_end_index):
+    def select_latent(self, latent, dim, start_index, end_index, use_end_index):
         samples = latent["samples"]
-        length = (samples.shape[2] - 1) * 4 + 1
-        
+        if dim >= len(samples.shape):
+            raise ValueError(f"Dimension {dim} is out of bounds for latent with shape {samples.shape}")
+        length = samples.shape[dim] - 1
+
         if start_index >= length:
             raise ValueError(f"Start index {start_index} is out of bounds for latent with length {length}")
-        # if end_index > length:
-        #     raise ValueError(f"End index {end_index} is out of bounds for latent with length {length}")
-        # if start_index >= end_index:
-        #     raise ValueError(f"Start index {start_index} must be less than end index {end_index}")
-        # if end_index % 4 != 0:
-        #     raise ValueError(f"End index {end_index} must be a multiple of 4")
-        
-        start_sample_index = int(start_index / 4)
 
-        # if start_sample_index == end_sample_index:
-        #     raise ValueError(f"Start and end sample indices {start_sample_index} must be different")
         if use_end_index:
             if end_index < start_index:
                 raise ValueError(f"End index {end_index} must be greater than or equal to start index {start_index}")
-            end_sample_index = int(end_index / 4) + 1
+            end_sample_index = int(end_index)
         else:
-            end_sample_index = samples.shape[2]
+            end_sample_index = samples.shape[dim]
 
-        selected = samples[:, :, start_sample_index:end_sample_index, :, :]
-        
-        selected_count = selected.shape[2] * 4 - 3  # Convert back to original length
+        selected = samples.index_select(dim, torch.arange(start_index, end_sample_index))
+
+        selected_count = selected.shape[dim]
         print("Selected latent shape:", selected.shape)
-        
+
         return ({"samples": selected}, selected_count)
